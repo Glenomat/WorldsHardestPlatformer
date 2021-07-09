b3fc9ecf0d60ee4c920959cac557c4485f0e1894
Glenomat <69582151+Glenomat@users.noreply.github.com>
2021-07-09 13:12:09 +0200

    Added Better In-Game Timer
    Old timer got changed to an time.deltaTime timer so that no seconds get skipped when Scene gets Reloaded or Skiped

--- Assets/Scripts/Menu/ScoreScreen.cs
+++ Assets/Scripts/Menu/ScoreScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,55 +11,41 @@ public class ScoreScreen : MonoBehaviour
     public Text lvlTxt;
     public Text timeTxt;
 
-    private static int sec = 0;
-    private static int minutes = 0;
-    private static int hours = 0;
-    private static string res = "00:00:00";
+    private static float time = 0f;
+    TimeSpan totalTime;
 
     void Start()
     {
-        if (SceneManager.GetActiveScene().name == "ScoreMenue") return;
-        if (SceneManager.GetActiveScene().buildIndex == 1)
+        if (SceneManager.GetActiveScene().name == "MainMenue")
         {
-            sec = 0;
-            minutes = 0;
-            hours = 0;
+            time = 0f;
+            return;
+        }
+        if(SceneManager.GetActiveScene().name == "FinalScreen")
+        {
+            totalTime = TimeSpan.FromSeconds(time);
+            timeTxt.text = "You'r final time is: " + String.Format("{0:00}:{1:00}:{2:00}", totalTime.Hours, totalTime.Minutes, totalTime.Seconds);
+            time = 0f;
+            return;
         }
         lvlTxt.text = "Level " + SceneManager.GetActiveScene().buildIndex;
-        StartCoroutine(TimeCalc());
     }
 
-    IEnumerator TimeCalc()
+    void Update()
     {
-        res = "";
-        sec++;
-        if (sec == 60)
-        {
-            sec = 0;
-            minutes++;
-        }
-        if (minutes == 60)
-        {
-            minutes = 0;
-            hours++;
-        }
+        if (SceneManager.GetActiveScene().name == "FinalScreen" || SceneManager.GetActiveScene().name == "MainMenue") return;
+        time += Time.deltaTime;
+        totalTime = TimeSpan.FromSeconds(time);
+        timeTxt.text = "Time: " + String.Format("{0:00}:{1:00}:{2:00}", totalTime.Hours, totalTime.Minutes, totalTime.Seconds);
+    }
 
-        if (hours >= 10)
-            res += hours + ":";
-        else
-            res += "0" + hours + ":";
-        if (minutes >= 10)
-            res += minutes + ":";
-        else
-            res += "0" + minutes + ":";
-        if (sec >= 10)
-            res += sec;
-        else
-            res += "0" + sec;
+    public void MainMenue()
+    {
+        SceneManager.LoadScene(0);
+    }
 
-        timeTxt.text = res;
-        PlayerPrefs.SetString("Time", res);
-        yield return new WaitForSecondsRealtime(1);
-        StartCoroutine(TimeCalc());
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
     }
 }
